61ec4ab3f648c42fb1971841f7e0947143e4d472
dulip-sameera <dsgitrepo101@gmail.com>
2022-10-01 19:07:54 +0530

    added display function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+const MAXIMUM_DIGITS_ALLOWED = 14;
+
 // creating the calculator object
 const calculator = {
   add(num1, num2) {
@@ -29,3 +31,25 @@ const calculator = {
     }
   },
 };
+
+//display the digits in the calculator display
+function display(string) {
+  displayContent.textContent = "";
+  displayContent.textContent = string;
+}
+
+let equationString = "";
+const operators = ["+", "-", "/", "*"];
+const nums = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
+//listen for a click in the window
+window.addEventListener("click", function (e) {
+  //get the element clicked
+  let dataKey = e.target.attributes["data-key"].value;
+  if (operators.includes(dataKey) || nums.includes(dataKey)) {
+    if (equationString.length !== MAXIMUM_DIGITS_ALLOWED) {
+      equationString += e.target.attributes["data-key"].value;
+    }
+    display(equationString);
+  }
+});
