e140990771fde23519cf90029dd101d09942a525
dulip-sameera <dsgitrepo101@gmail.com>
2022-10-01 18:17:31 +0530

    added operate function

--- js/main.js
+++ js/main.js
@@ -15,4 +15,17 @@ const calculator = {
   divide(num1, num2) {
     return num2 === 0 ? NaN : num1 / num2;
   },
+  // function that take 2 numbers and an operator and operate on them
+  operate(num1, num2, operator) {
+    switch (operator) {
+      case "+":
+        return calculator.add(num1, num2);
+      case "-":
+        return calculator.subtract(num1, num2);
+      case "*":
+        return calculator.multiply(num1, num2);
+      case "/":
+        return calculator.divide(num1, num2);
+    }
+  },
 };
