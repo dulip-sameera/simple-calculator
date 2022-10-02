d8319fa3aeb980308bf9d261c72a187346ef4972
dulip-sameera <dsgitrepo101@gmail.com>
2022-10-02 22:49:10 +0530

    added function add()

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// calculator functions
+
+// 1. add function
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+// 2. subtract function
+// 3. multiplication function
+// 4. division function
+
+// testing...
+console.log(add(-10, 5));
