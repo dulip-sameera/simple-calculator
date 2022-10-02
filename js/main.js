488c094a64ff3f3368473e468ba0bf4010ddcf36
dulip-sameera <dsgitrepo101@gmail.com>
2022-10-02 23:10:59 +0530

    added two functions divide(), multiply()

--- js/main.js
+++ js/main.js
@@ -7,11 +7,20 @@ function add(num1, num2) {
 
 // 2. subtract function
 function subtract(num1, num2) {
-  return num1 - num2;
+  return num1 >= num2 ? num1 - num2 : num2 - num1;
 }
 
 // 3. multiplication function
-// 4. division function
+function multiply(num1, num2) {
+  return num1 * num2;
+}
 
+// 4. division function
+function divide(num1, num2) {
+  if (num1 === 0 && num2 === 0) {
+    return NaN;
+  }
+  return num2 !== 0 ? num1 / num2 : "Not Divisible By Zero";
+}
 // testing...
-console.log(subtract(-10, 5));
+console.log(divide(0, 0));
